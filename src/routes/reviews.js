ad624dcd1974fb1cfef5709e1942b6a6437e3b8d
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-10-18 15:55:37 -0700

    added images, few typos fixed

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 router.route('/:title/reviews/new')
   .get((req, res) => {
     albums.getByTitle(req.params.title)
-      .then(album => res.render('new-review', {album}))
+      .then(album => res.render('new_review', {album}))
       .catch(error => res.status(500).render('error', {error}))
   })
   .post((req, res) => {
